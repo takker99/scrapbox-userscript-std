448d044cd238f30ee808c761a4b425bc36a009a0
takker99 <37929109+takker99@users.noreply.github.com>
2022-02-10 12:22:55 +0900

    :sparkles: Add sleep and range

---
+++ range.test.ts
@@ -0,0 +1,22 @@
+import { range } from "./range.ts";
+import { assertStrictEquals } from "./deps/testing.ts";
+
+Deno.test("range()", () => {
+  let count = 0;
+  for (const i of range(0, 10)) {
+    assertStrictEquals<number>(i, count++);
+  }
+  assertStrictEquals<number>(10, count);
+
+  count = 5;
+  for (const i of range(5, 12)) {
+    assertStrictEquals<number>(i, count++);
+  }
+  assertStrictEquals<number>(12, count);
+
+  count = 0;
+  for (const i of range(5, 5)) {
+    assertStrictEquals<number>(i, count++);
+  }
+  assertStrictEquals<number>(0, count);
+});
