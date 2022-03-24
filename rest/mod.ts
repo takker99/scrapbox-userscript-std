49f0423e18ce80553c539829efc3405af0190bf5
takker99 <37929109+takker99@users.noreply.github.com>
2022-03-25 01:48:58 +0900

    :+1: re-export from mod.ts

--- rest/mod.ts
+++ rest/mod.ts
@@ -3,3 +3,6 @@ export * from "./project.ts";
 export * from "./profile.ts";
 export * from "./replaceLinks.ts";
 export * from "./page-data.ts";
+export * from "./auth.ts";
+export * from "./util.ts";
+export * from "./error.ts";
