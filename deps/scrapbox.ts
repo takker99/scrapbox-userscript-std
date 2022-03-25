f0fbb59592f1a63a8ed9eeb04929a0518ef79f4f
takker99 <37929109+takker99@users.noreply.github.com>
2022-03-25 20:28:32 +0900

    :sparkles: Implement wrappers of cross searches

--- deps/scrapbox.ts
+++ deps/scrapbox.ts
@@ -7,6 +7,7 @@ export type {
   InvalidURLError,
   MemberProject,
   MemberUser,
+  NoQueryError,
   NotFoundError,
   NotLoggedInError,
   NotMemberError,
@@ -14,11 +15,13 @@ export type {
   NotPrivilegeError,
   Page,
   PageList,
+  ProjectSearchResult,
   Scrapbox,
   SearchedTitle,
+  SearchResult,
   SessionError,
   TweetInfo,
-} from "https://raw.githubusercontent.com/scrapbox-jp/types/0.1.2/mod.ts";
-import type { Page } from "https://raw.githubusercontent.com/scrapbox-jp/types/0.1.2/mod.ts";
+} from "https://raw.githubusercontent.com/scrapbox-jp/types/0.1.4/mod.ts";
+import type { Page } from "https://raw.githubusercontent.com/scrapbox-jp/types/0.1.4/mod.ts";
 export * from "https://esm.sh/@progfay/scrapbox-parser@7.2.0";
 export type Line = Page["lines"][0];
--- rest/mod.ts
+++ rest/mod.ts
@@ -4,6 +4,7 @@ export * from "./profile.ts";
 export * from "./replaceLinks.ts";
 export * from "./page-data.ts";
 export * from "./link.ts";
+export * from "./search.ts";
 export * from "./getWebPageTitle.ts";
 export * from "./getTweetInfo.ts";
 export * from "./auth.ts";
