373d3a954ad433f1744e19767ae0e522e502c485
takker99 <37929109+takker99@users.noreply.github.com>
2022-05-01 20:40:59 +0900

    :arrow_up:

--- deps/scrapbox.ts
+++ deps/scrapbox.ts
@@ -1,8 +1,8 @@
 export type {
   Line,
   Scrapbox,
-} from "https://raw.githubusercontent.com/scrapbox-jp/types/0.3.3/userscript.ts";
+} from "https://raw.githubusercontent.com/scrapbox-jp/types/0.3.4/userscript.ts";
 export type {
   BaseStore,
-} from "https://raw.githubusercontent.com/scrapbox-jp/types/0.3.3/baseStore.ts";
+} from "https://raw.githubusercontent.com/scrapbox-jp/types/0.3.4/baseStore.ts";
 export * from "https://esm.sh/@progfay/scrapbox-parser@7.2.0";
