ff15f2fa7007f46664f513a4084f580f8f03c67d
takker99 <37929109+takker99@users.noreply.github.com>
2022-05-13 16:30:23 +0900

    :boom::fix: Get both Cursor and Selection at once

--- browser/dom/stores.ts
+++ browser/dom/stores.ts
@@ -6,7 +6,7 @@ import { textInput } from "./dom.ts";
 import { Cursor } from "./cursor.d.ts";
 import { Selection } from "./selection.d.ts";
 
-export const takeStores = (): (Cursor | Selection)[] => {
+export const takeStores = (): { cursor: Cursor; selection: Selection } => {
   const textarea = textInput();
   if (!textarea) {
     throw Error(`#text-input is not found.`);
@@ -21,9 +21,24 @@ export const takeStores = (): (Cursor | Selection)[] => {
   }
 
   // @ts-ignore DOMを無理矢理objectとして扱っている
-  return (textarea[
+  const stores = (textarea[
     reactKey
-  ] as ReactFiber).return.return.stateNode._stores;
+  ] as ReactFiber).return.return.stateNode._stores as (Cursor | Selection)[];
+
+  const cursor = stores.find((store) =>
+    store.constructor.name === "Cursor"
+  ) as (Cursor | undefined);
+  if (!cursor) {
+    throw Error('#text-input must has a "Cursor" store.');
+  }
+  const selection = stores.find((store) =>
+    store.constructor.name === "Selection"
+  ) as (Selection | undefined);
+  if (!selection) {
+    throw Error('#text-input must has a "Selection" store.');
+  }
+
+  return { cursor, selection };
 };
 
 interface ReactFiber {
