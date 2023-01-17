4c2ac654a3791c1c404b2bd18c7c6769589a6335
GitHub <noreply@github.com>
2023-01-17 00:44:42 +0000

    :arrow_up: update deno dependencies

--- deps/testing.ts
+++ deps/testing.ts
@@ -1,2 +1,2 @@
-export * from "https://deno.land/std@0.172.0/testing/asserts.ts";
-export * from "https://deno.land/std@0.172.0/testing/snapshot.ts";
+export * from "https://deno.land/std@0.173.0/testing/asserts.ts";
+export * from "https://deno.land/std@0.173.0/testing/snapshot.ts";
