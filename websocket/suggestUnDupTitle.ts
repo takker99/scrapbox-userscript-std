c7f94bec4210f17970e8cee1b32fe87fa7abd887
takker99 <37929109+takker99@users.noreply.github.com>
2025-04-15 14:25:03 +0900

    refactor(test): Move `suggestUnDupTitle` tests into JSDoc comments

--- websocket/suggestUnDupTitle.ts
+++ websocket/suggestUnDupTitle.ts
@@ -1,3 +1,19 @@
+/**
+ * Suggest a new title that is already in use.
+ *
+ * ```ts
+ * import { assertEquals } from "@std/assert/equals";
+ *
+ * assertEquals(suggestUnDupTitle("title"), "title_2");
+ * assertEquals(suggestUnDupTitle("title_2"), "title_3");
+ * assertEquals(suggestUnDupTitle("title_10"), "title_11");
+ * assertEquals(suggestUnDupTitle("title_10_3"), "title_10_4");
+ * assertEquals(suggestUnDupTitle("another_title_5"), "another_title_6");
+ * ```
+ *
+ * @param title - The title to suggest a new name for
+ * @returns
+ */
 export const suggestUnDupTitle = (title: string): string => {
   const matched = title.match(/(.+?)(?:_(\d+))?$/);
   const title_ = matched?.[1] ?? title;
