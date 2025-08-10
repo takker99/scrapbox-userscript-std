fa9d8012ba848446a675c0ef051fd82a2eaa1447
takker99 <37929109+takker99@users.noreply.github.com>
2025-08-10 20:15:12 +0900

    feat(api): add smart context endpoints for 1-hop and 2-hop links

--- api.ts
+++ api.ts
@@ -1,5 +1,6 @@
 export * from "./api/pages.ts";
 export * from "./api/projects.ts";
 export * from "./api/users.ts";
+export * from "./api/smart-context.ts";
 export type { HTTPError, TypedError } from "./error.ts";
 export type { BaseOptions, ExtendedOptions, OAuthOptions } from "./util.ts";
