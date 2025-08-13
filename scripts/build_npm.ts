b8f04fd15067f38acfcd96334cf7f6173ebc5303
takker99 <37929109+takker99@users.noreply.github.com>
2025-08-13 18:53:17 +0900

    fix(package): ensure all submodule paths are included in the npm package

--- scripts/build_npm.ts
+++ scripts/build_npm.ts
@@ -1,4 +1,5 @@
 import { build, emptyDir } from "@deno/dnt";
+import { parse } from "@std/jsonc";
 
 await emptyDir("./npm");
 
@@ -18,101 +19,16 @@ await Deno.writeTextFile(
   ),
 );
 
+const config = parse(await Deno.readTextFile("./deno.jsonc")) as {
+  exports: Record<string, string>;
+};
+
 await build({
   entryPoints: [
     "./mod.ts",
-    {
-      name: "./browser",
-      path: "./browser/mod.ts",
-    },
-    {
-      name: "./browser/dom",
-      path: "./browser/dom/mod.ts",
-    },
-    {
-      name: "./browser/websocket",
-      path: "./websocket/mod.ts",
-    },
-    {
-      name: "./parseAbsoluteLink",
-      path: "./parseAbsoluteLink.ts",
-    },
-    {
-      name: "./rest",
-      path: "./rest/mod.ts",
-    },
-    {
-      name: "./text",
-      path: "./text.ts",
-    },
-    {
-      name: "./title",
-      path: "./title.ts",
-    },
-    {
-      name: "./websocket",
-      path: "./websocket/mod.ts",
-    },
-    {
-      name: "./unstable-api",
-      path: "./api.ts",
-    },
-    {
-      name: "./unstable-api/pages",
-      path: "./api/pages.ts",
-    },
-    {
-      name: "./unstable-api/pages/project",
-      path: "./api/pages/project.ts",
-    },
-    {
-      name: "./unstable-api/pages/project/replace",
-      path: "./api/pages/project/replace.ts",
-    },
-    {
-      name: "./unstable-api/pages/project/replace/links",
-      path: "./api/pages/project/replace/links.ts",
-    },
-    {
-      name: "./unstable-api/pages/project/search",
-      path: "./api/pages/project/search.ts",
-    },
-    {
-      name: "./unstable-api/pages/project/search/query",
-      path: "./api/pages/project/search/query.ts",
-    },
-    {
-      name: "./unstable-api/pages/project/search/titles",
-      path: "./api/pages/project/search/titles.ts",
-    },
-    {
-      name: "./unstable-api/pages/project/title",
-      path: "./api/pages/project/title.ts",
-    },
-    {
-      name: "./unstable-api/pages/projects",
-      path: "./api/projects.ts",
-    },
-    {
-      name: "./unstable-api/pages/projects/project",
-      path: "./api/projects/project.ts",
-    },
-    {
-      name: "./unstable-api/pages/project/title/text",
-      path: "./api/pages/project/title/text.ts",
-    },
-    {
-      name: "./unstable-api/pages/project/title/icon",
-      path: "./api/pages/project/title/icon.ts",
-    },
-    {
-      name: "./unstable-api/users",
-      path: "./api/users.ts",
-    },
-    {
-      name: "./unstable-api/users/me",
-      path: "./api/users/me.ts",
-    },
+    ...Object.entries(config.exports).flatMap(
+      ([name, path]) => name === "." ? [] : [{ name, path }],
+    ),
   ],
   outDir: "./npm",
   shims: { deno: "dev" },
