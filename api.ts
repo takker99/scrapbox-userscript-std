25a6ec3cff299b44c4fe48dcd724aba95b8e1296
takker99 <37929109+takker99@users.noreply.github.com>
2025-08-18 11:32:18 +0900

    feat: Implement `/api/commits/:project/:pageid`
    Only in `/unstable-api`

--- api.ts
+++ api.ts
@@ -1,6 +1,7 @@
+export * from "./api/commits.ts";
 export * from "./api/pages.ts";
 export * from "./api/projects.ts";
-export * from "./api/users.ts";
 export * from "./api/smart-context.ts";
+export * from "./api/users.ts";
 export type { HTTPError, TypedError } from "./error.ts";
 export type { BaseOptions, ExtendedOptions, OAuthOptions } from "./util.ts";
